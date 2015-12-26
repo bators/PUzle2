98c3ca8e05ba3f09225f87559e981d9d114fddf9
bators <artiym9696@gmail.ru>
2015-12-26 15:45:29 +0300

    asdasdasa

---
+++ Puzzle/Puzzle/Model/Fragment.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+namespace Puzzle.Model
+{
+    public class Fragment :ViewModelBase
+    {
+
+
+        private int _ind;
+        public int ind
+        {
+            get
+            {
+                return _ind;
+            }
+            set
+            {
+                _ind=value;
+            }
+        }
+
+ 
+        private Bitmap _Pic;
+        public Bitmap Pic
+        {
+            get 
+            { 
+
+                return _Pic;
+            }
+            set
+            {
+                if (_Pic != value)
+                {
+                    _Pic = value;
+                    RaisePropertyChanged("Pic");
+
+                }
+            }
+
+        }
+       
+        
+        
+    }
+}
---
+++ Puzzle/Puzzle/ViewModel/Drop.cs
@@ -0,0 +1,89 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Puzzle.Model;
+using System.Drawing;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.Collections.ObjectModel;
+namespace Puzzle.ViewModel
+{
+   public  class Drop:ViewModelBase
+   {
+       public Drop(Fragment frag)
+        {
+            if (frag == null) throw new ArgumentNullException("Image");
+            _frag = frag;
+            RaisePropertyChanged("Image");
+           
+        }
+       
+       
+       private Fragment _frag;
+       public Bitmap Image
+       {
+           get
+           {
+               if (_frag!= null)
+                   return _frag.Pic;
+
+               return null;
+           }
+           set 
+           {
+               _frag.Pic = value;
+           }
+
+       }
+
+       private RelayCommand<DragEventArgs> _ImageDrop;
+       public RelayCommand<DragEventArgs> ImageDrop
+        {
+            get
+            {
+                if (_ImageDrop == null)
+
+                    _ImageDrop = new RelayCommand<DragEventArgs>(hatDropExecute);
+
+                return _ImageDrop;
+            }
+        }
+
+
+        private void hatDropExecute( DragEventArgs e)
+        {
+            Fragment vm = e.Data.GetData(typeof(Fragment)) as Fragment;
+            if (vm != null) return;
+            _frag= vm;
+            RaisePropertyChanged("Image");
+            
+        }
+
+        private RelayCommand<MouseButtonEventArgs> _CanvasMouseDown;
+        public RelayCommand<MouseButtonEventArgs> CanvasMouseDown
+        {
+            get
+            {
+                if (_CanvasMouseDown == null)
+                    _CanvasMouseDown = new RelayCommand<MouseButtonEventArgs>(executeListBoxMouseDown);
+
+                return _CanvasMouseDown;
+            }
+        }
+
+        private void executeListBoxMouseDown(MouseButtonEventArgs e)
+        {
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                _frag = null;
+                RaisePropertyChanged("Image");
+            }
+        }
+
+    }
+}
